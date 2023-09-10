2d7061411420c0a694356bc8ee66a35cdfc92cc9
Antti Juustila <antti.juustila@gmail.com>
2023-09-10 20:42:14 +0300

    Java and Swift samples in same repo

---
+++ swift/WuTangNameTestTarget/WuTangNameTestTarget.swift
@@ -0,0 +1,44 @@
+//
+//  WuTangNameTestTarget.swift
+//  WuTangNameTestTarget
+//
+//  Created by Antti Juustila on 1.9.2023.
+//
+
+import XCTest
+import WuTangFunctional
+
+final class WuTangNameTestTarget: XCTestCase {
+
+	override func setUpWithError() throws {
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+
+	override func tearDownWithError() throws {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+	}
+
+	func testWuTangNameUniqueness() throws {
+		let names =
+		[
+			"Antti J",
+			"Jouni L",
+			"Joni R",
+			"Kristian H",
+			"Pertti K",
+			"Markus K",
+			"Päivi R-J",
+			"Tim C",
+			"Bill G"
+		]
+
+		let programmers = createProgrammers(for: names).thenAssignWuTangNames()
+		XCTAssertEqual(names.count, programmers.them.count, "All names must get programmer names")
+		var wuTangNames = Set<String>()
+		for programmer in programmers.them where programmer.wutangName != nil {
+			let result = wuTangNames.insert(programmer.wutangName!)
+			XCTAssertTrue(result.inserted, "\(programmer.wutangName!) not inserted since it is already in use")
+		}
+	}
+
+}
